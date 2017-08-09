de8182aa9d255650b70648a160068bd8085ea86c
Unknown <vmalab@foi.hr>
2017-08-09 13:25:39 +0200

    Added MainFrm

--- EasyStorage/EasyStorage/MainFrm.Designer.cs
+++ EasyStorage/EasyStorage/MainFrm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EasyStorage
 {
-    partial class Form1
+    partial class MainFrm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "MainFrm";
+            this.Text = "EasyStorage";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- EasyStorage/EasyStorage/MainFrm.cs
+++ EasyStorage/EasyStorage/MainFrm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EasyStorage
 {
-    public partial class Form1 : Form
+    public partial class MainFrm : Form
     {
-        public Form1()
+        public MainFrm()
         {
             InitializeComponent();
         }
